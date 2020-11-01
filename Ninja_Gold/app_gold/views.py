c2e3041c837887f94e70ab7ef4d84ff703e934de
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-01 15:42:29 -0800

    Added the function to generate a random amount of gold and increment the total then display it in the user's total gold on index.html

--- Ninja_Gold/app_gold/views.py
+++ Ninja_Gold/app_gold/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+import random
 # Create your views here.
 def index(request):
+    if 'views' in request.session:
+        request.session['views'] += 1
+    else:
+        request.session['views'] = 1
+        request.session['userTotal'] = 0
+        request.session['history'] = ''
     return render(request, 'index.html')
+
+def process_money(request):
+    # farm
+    if int(request.POST['findGold']) == 1:
+        print('farm')
+        goldFound = random.randint(10,20)
+        print(goldFound)
+        request.session['userTotal'] += goldFound
+    # cave
+    if int(request.POST['findGold']) == 2:
+        print('cave')
+        goldFound = random.randint(5,10)
+        print(goldFound)
+        request.session['userTotal'] += goldFound
+    # house
+    if int(request.POST['findGold']) == 3:
+        print('house')
+        goldFound = random.randint(2,5)
+        print(goldFound)
+        request.session['userTotal'] += goldFound
+    # casino
+    if int(request.POST['findGold']) == 4:
+        print('casino')
+        goldFound = random.randint(0,50)
+        print(goldFound)
+        request.session['userTotal'] += goldFound
+    return redirect('/')
