c1c03ee1343e6b91bee6b0370467f5bc5e8fb88a
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-01 14:22:10 -0800

    link root to index.html

--- Ninja_Gold/app_gold/views.py
+++ Ninja_Gold/app_gold/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index(repuest):
-    return HttpResponse('Can you see me Now?!')
+def index(request):
+    return render(request, 'index.html')
